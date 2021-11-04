9d05a32b1f7310b3f3f97eb45fb6ed7278e4b544
mohamedamineayadi <47778318+mohamedamineayadi@users.noreply.github.com>
2021-11-04 15:23:09 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express  = require('express')
+const mongoose = require('mongoose')
+const morgan   = require('morgan')
+const bodyParser = require('body-parser')
+
+
+mongoose.connect('mongodb://localhost:27017/MemeGenerator',{useNewUrlParser: true,useUnifiedTopology: true})
+const db = mongoose.connection
+
+db.on('error',(err) => {
+   console.log(err)
+
+})
+
+db.once('open',() => {
+
+   console.log('DataBase Connection Established !')
+
+})
+
+
+const app = express ()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=> {
+    console.log(`Server  is Running  on PORT ${PORT}`)
+    
+
+})
