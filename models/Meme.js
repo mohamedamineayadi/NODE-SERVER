7bf675ccaaa59eba68138b8ce7cae87607d9722b
mohamedamineayadi <47778318+mohamedamineayadi@users.noreply.github.com>
2021-11-12 21:44:53 +0100

    Add files via upload

---
+++ models/Meme.js
@@ -0,0 +1,19 @@
+var mongoose=require('mongoose')
+var Schema =mongoose.Schema
+
+const meme = new Schema({
+        text :{ 
+                type:String
+        },
+        image :{
+                type:String
+        
+        },
+        
+
+
+
+},{timestamps:true})
+
+const Meme =mongoose.model('Meme',meme)
+module.exports = Meme
